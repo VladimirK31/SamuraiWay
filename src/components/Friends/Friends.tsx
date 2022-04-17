4ba8a62307d4768ec2bdd6207588f0d8ba8c38e0
VladimirK31 <93672836+VladimirK31@users.noreply.github.com>
2022-04-17 12:31:13 +0300

    new

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import state from '../../Redux/State'
 import s from './Friends.module.css'
 function Friends(props: any) {
-  debugger
-  let friendsElements = props.state.map(
-    (f: { name: any; images: any }) => f.name
-  )
+  let friendsElements = props.state.map((f: { name: any }) => f.name)
   return (
     <div>
-      <div>Friends</div>
-      <div className={s.frinds}> {friendsElements}</div>
+      <h3>Friends</h3>
+      <div className={s.friendsElements}>{friendsElements}</div>
     </div>
   )
 }
